969e6fd1769b103f8f896b47dec737e190150d0d
Tirth5352 <Tirth5253>
2026-01-07 19:40:26 +0530

    fix: Version update and fix

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -1,31 +1,31 @@
-'use client';
+"use client";
 
-import { Menu,  } from "lucide-react";
+import { Menu } from "lucide-react";
 
 import { Button } from "./ui/button";
-import {Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
 import Sidebar from "./sidebar";
 import { useEffect, useState } from "react";
 
-const MobileSidebar=()=>{
-    const [isMounted,setIsMounted]=useState(false);
-    useEffect(()=>{
-setIsMounted(true);
-    },[])
-    if(!isMounted){
-        return null;
-    }
-return(
+const MobileSidebar = () => {
+  const [isMounted, setIsMounted] = useState(false);
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+  if (!isMounted) {
+    return null;
+  }
+  return (
     <Sheet>
-    <SheetTrigger>
-    <Button variant="ghost" size="icon" className="md:hidden">
-          <Menu/>
-      </Button> 
+      <SheetTrigger>
+        <Button variant="ghost" size="icon" className="md:hidden">
+          <Menu />
+        </Button>
       </SheetTrigger>
       <SheetContent side="left" className="p-0">
-      <Sidebar/>
+        <Sidebar apiLimitCount={5} isPro={false} />
       </SheetContent>
-      </Sheet>
-)
-}
-export default MobileSidebar
+    </Sheet>
+  );
+};
+export default MobileSidebar;
